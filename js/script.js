069dfe6c81b88f911de885b5182efcea3a2e42e0
Yvonne <giryvonne@gmail.com>
2019-08-02 16:27:02 +0200

    add some changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-$(document).submit(function(){
-    $("#click").click(function(){
-    $("#h3").toggle();
-    $("#menu").toggle();
-    $("#form-order").toggle();
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-    });
-});
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1} 
+//   if (n < 1) {slideIndex = x.length} ;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none"; 
+//   }
+//   x[slideIndex-1].style.display = "block"; 
+// }
+
+//Business logggic
