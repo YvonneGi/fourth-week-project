fb07b0ec47af61c47a548981ed25fb84f8f243f1
Yvonne <giryvonne@gmail.com>
2019-08-02 17:52:32 +0200

    add changes in script file

--- js/script.js
+++ js/script.js
@@ -16,4 +16,49 @@
 //   x[slideIndex-1].style.display = "block"; 
 // }
 
-//Business logggic
+//Business logic
+function Order(flavour,topping,crust,size,number){
+    this.myFlavour = flavour;
+    this.myTopping = topping;
+    this.myCrust = crust;
+    this.mySize = size;
+    this.myNumber = number;
+}
+function resetFields(){
+    $("#flavour").val("");
+    $("#size").val("");
+    $("#topping").val("");
+    $("#crust").val("");
+    $("#new-number").val("");
+}
+//User Interface logic
+$(document).ready(function(){
+    $("#order-pizza").submit(function(event){
+        event.preventDefault();
+        var newFlavour = $ ("#flavour").val();
+        var newSize = $ ("#size").val();
+        var newTopping = $ ("#topping").val();
+        var newCrust = $ ("#crust").val();
+        var newNumber = $ ("#new-number").val();
+        var yourOrder = $ ("#your-order").val();
+        var newOrder = new Order(yourOrder,newFlavour,newSize,newTopping,newCrust,newNumber);
+    });
+    $("ul#your-order").append("<li><span class='orders'>" + newOrder.newFlavour() + "</span></li>");
+    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newSize() + "</span></li>");
+    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newTopping() + "</span></li>");
+    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newCrust() + "</span></li>");
+    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newNumber() + "</span></li>");
+    
+    $(".orders").last().click(function(){
+        {
+            $("#show-order").show();
+            $(".flavour").text(newOrder.flavour);
+            $(".size").text(newOrder.size);
+            $(".topping").text(newOrder.topping);
+            $(".crust").text(newOrder.crust);
+            $(".new-number").number(newOrder.number);
+
+        }
+    });
+    resetFields();
+});
