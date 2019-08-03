80214a26a30deb048d63ae4d3d35189443578b0d
Yvonne <giryvonne@gmail.com>
2019-08-03 18:50:42 +0200

    add changes

--- js/script.js
+++ js/script.js
@@ -16,49 +16,54 @@
 //   x[slideIndex-1].style.display = "block"; 
 // }
 
-//Business logic
-function Order(flavour,topping,crust,size,number){
+function Order(flavour,size,topping,crust,number) {
     this.myFlavour = flavour;
+    this.mySize = size;
     this.myTopping = topping;
     this.myCrust = crust;
-    this.mySize = size;
     this.myNumber = number;
-}
-function resetFields(){
-    $("#flavour").val("");
-    $("#size").val("");
-    $("#topping").val("");
-    $("#crust").val("");
-    $("#new-number").val("");
-}
-//User Interface logic
-$(document).ready(function(){
-    $("#order-pizza").submit(function(event){
-        event.preventDefault();
-        var newFlavour = $ ("#flavour").val();
-        var newSize = $ ("#size").val();
-        var newTopping = $ ("#topping").val();
-        var newCrust = $ ("#crust").val();
-        var newNumber = $ ("#new-number").val();
-        var yourOrder = $ ("#your-order").val();
-        var newOrder = new Order(yourOrder,newFlavour,newSize,newTopping,newCrust,newNumber);
-    });
-    $("ul#your-order").append("<li><span class='orders'>" + newOrder.newFlavour() + "</span></li>");
-    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newSize() + "</span></li>");
-    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newTopping() + "</span></li>");
-    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newCrust() + "</span></li>");
-    // $("ul#your-order").append("<li><span class='orders'>" + newOrder.newNumber() + "</span></li>");
-    
-    $(".orders").last().click(function(){
-        {
-            $("#show-order").show();
-            $(".flavour").text(newOrder.flavour);
-            $(".size").text(newOrder.size);
-            $(".topping").text(newOrder.topping);
-            $(".crust").text(newOrder.crust);
-            $(".new-number").number(newOrder.number);
+  }
+  
+  Order.prototype.fullOrder = function() {
+    return this.myFlavour + " " + this.mySize + " " + this.myTopping + " " + this.myCrust + " " + this.myNumber;
+  }
+  // function resetFields() {
+  //     $("select#flavour").val("");
+  //     $("select#size").val("");
+  //     $("select#topping").val("");
+  //     $("select#crust").val("");
+  //     $("select#new-number").val("");
+  // }
+  
+  // user interface logic
+  $(document).ready(function() {
+    $("form#order-pizza").submit(function(event) {
+      event.preventDefault();
+  
+      var inputtedFlavour = $("select#flavour").val();
+      var inputtedSize = $("select#size").val();
+      var inputtedTopping = $("select#topping").val();
+      var inputtedCrust = $("select#crust").val();
+      var inputtedNumber = $("select#new-number").val();
 
-        }
+
+      var newOrder = new Order(inputtedFlavour, inputtedSize, inputtedTopping, inputtedCrust, inputtedNumber);
+      console.log(newOrder);
+      $("ul#our-order").append("<li><span class='contact'>" + newOrder.fullOrder() + "</span></li>");
+  
+      $(".contact").last().click(function() {
+        $("#show-order").show();
+        $("#show-order h2").text(newOrder.fullOrder());
+        $(".flavour").text(newOrder.flavour);
+        $(".size").text(newOrder.size);
+        $(".topping").text(newOrder.topping);
+        $(".crust").text(newOrder.crust);
+        $(".number").text(newOrder.new-number);
+
+      });
+  
+      // resetFields();
+  
     });
-    resetFields();
-});
+  });
+  
